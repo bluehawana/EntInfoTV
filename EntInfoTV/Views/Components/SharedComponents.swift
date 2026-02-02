b2d826455799cc9c21ff5a64793f2c9eb555645a
bluehawana <bluehawana@gmail.com>
2026-02-02 12:22:43 +0100

    Increase brand logo size

--- EntInfoTV/Views/Components/SharedComponents.swift
+++ EntInfoTV/Views/Components/SharedComponents.swift
@@ -155,7 +155,7 @@ struct BrandWordmark: View {
         Image("AppLogo")
             .resizable()
             .scaledToFit()
-            .frame(height: 32)
+            .frame(height: 48)
             .shadow(color: .black.opacity(0.4), radius: 6, x: 0, y: 2)
             .focusable(false)
             .accessibilityLabel("EntInfo")
