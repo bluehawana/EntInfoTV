b0b15d89b86670c1fe1ed0d90b3ca87f948af619
bluehawana <bluehawana@gmail.com>
2026-02-02 13:47:31 +0100

    Lift navbar logo

--- EntInfoTV/Views/RootTabView.swift
+++ EntInfoTV/Views/RootTabView.swift
@@ -32,7 +32,7 @@ struct RootTabView: View {
         .overlay(alignment: .topLeading) {
             BrandWordmark()
                 .padding(.leading, 56)
-                .padding(.top, -8)
+                .padding(.top, -22)
                 .allowsHitTesting(false)
         }
     }
